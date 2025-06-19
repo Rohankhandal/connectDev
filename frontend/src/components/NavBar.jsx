992051a1641d5a4e04242a381a1297f703a0b227
Rohankhandal <rohankhandal84@gmail.com>
2025-06-19 23:43:00 +0530

    Edit Profile feature

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,3 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
